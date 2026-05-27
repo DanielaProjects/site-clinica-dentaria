c24a330d6c098dd3f6de59625d792557eccece8a
Daniela Teixeira <danielatex200501@gmail.com>
2026-05-27 21:14:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function login(){
 
+    const utilizador = document.getElementById("utilizador").value;
+
     const pin = document.getElementById("pin").value;
 
     if(pin === "0793"){
+
+        localStorage.setItem(
+            "utilizador",
+            utilizador
+        );
 
         window.location.href = "dashboard.html";
 
